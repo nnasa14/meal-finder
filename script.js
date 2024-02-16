ce5344cd1d9f66f4abcc19cb403dc482e51dc44f
nnasa14 <nnascimento.kr@gmail.com>
2024-02-16 15:34:33 -0500

    added fade effect to generated text

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
     let getMeal = () => {
+        mealsContainer.classList.remove('fade');
         fetch(url)
             .then(response => response.json())
             .then(data => {
                 const mealsName = data.meals[0].strMeal;
                 mealsContainer.textContent = `${mealsName}`;
+                mealsContainer.classList.add('fade')
             })
             .catch(error => console.error('Fetch error:', error));
     }
