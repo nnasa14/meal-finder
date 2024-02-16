79763d7cc1415f66987620760d60cf3a2479cc21
nnasa14 <nnascimento.kr@gmail.com>
2024-02-16 14:52:05 -0500

    successfully implemented API

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-const axios = require('axios');
-
-const options = {
-  method: 'GET',
-  url: 'www.themealdb.com/api/json/v1/1/random.php',
-};
-
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
+document.getElementById('meals');
+const btn = document.getElementById('btn');
+const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
 let getMeal = () => {
     fetch(url)
-    .then(data => data.json())
-    .then(item => console.log(item.meal));
+    .then(response => response.json())
+    .then(data => {
+    const mealName = data.meals[0].strMeal;
+    console.log(mealName);
+    })
+    .catch(error => console.error('Fetch error:'));
 }
 
 getMeal();
