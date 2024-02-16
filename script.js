9a570634d172458d70c15de1dd78ce396baa4dcd
nnasa14 <nnascimento.kr@gmail.com>
2024-02-16 10:32:57 -0500

    implementing question input

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const mealContainer = document.getElementById("meal");
-const btn = document.getElementById("btn");
-const url = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch';
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://webknox-recipes.p.rapidapi.com/recipes/quickAnswer',
+  params: {
+    q: 'How much vitamin c is in 2 apples?'
+  },
+  headers: {
+    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+    'X-RapidAPI-Host': 'webknox-recipes.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
 
 let getMeal = () => {
     fetch(url)
