b8c338adcfd21cbb520596f3a3e64ea606948172
nnasa14 <nnascimento.kr@gmail.com>
2024-02-16 15:32:01 -0500

    successfully implemented button to recall the function

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-document.getElementById('meals');
-const btn = document.getElementById('btn');
-const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
+document.addEventListener("DOMContentLoaded", function () {
+    const mealsContainer = document.getElementById('meals');
+    const btn = document.getElementById('btn');
+    const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
-let getMeal = () => {
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-    const mealName = data.meals[0].strMeal;
-    console.log(mealName);
-    })
-    .catch(error => console.error('Fetch error:'));
-}
+    let getMeal = () => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                const mealsName = data.meals[0].strMeal;
+                mealsContainer.textContent = `${mealsName}`;
+            })
+            .catch(error => console.error('Fetch error:', error));
+    }
 
-getMeal();
+    btn.addEventListener("click", getMeal);
+    getMeal();
+});
